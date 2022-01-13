055479db2f7717dc69aab79e13f26e5fd5f21032
av <snipfar.1@gmail.com>
2022-01-13 01:00:35 -0800

    error handeling

--- gui.py
+++ gui.py
@@ -2,46 +2,64 @@ from tkinter import *
 import tkinter.font as font
 import subprocess 
 
-
-
+# you're going to need to refacter this # 
 
 recordClicked = False 
-recordPID = ''
 
-p = 0
 
-def startFilming():  
+def startOrStopFilming():  
   global recordClicked 
-  global recordPID
-  global p 
-
-  if recordClicked==False: 
-    p = subprocess.Popen('./scripts/record_video.sh', shell=True) 
-    recordClicked = True 
  
+  if not recordClicked:  
+    p = subprocess.Popen('./scripts/record_video.sh', shell=True)
+    recordClicked = True 
+    videoButton['text'] = 'Click To Stop Filming' 
+    #resets the uploading text on new action
+    li['text'] = '' 
   else:
     subprocess.run('kill $(pgrep gphoto2)', shell=True) 
+    recordClicked = False
+    videoButton['text'] = 'Click To Start Filming'
 
-#def startPodcast(): 
 
-def main(): 
-  gui = Tk()
-  gui.geometry("1920x1080")
 
-  l = Label(gui, text = "Auto Filmer and Podcaster Desgined by Avery :D")
-  l.config(font =("Courier", 14))
-  # create button
-  videoButton = Button(gui, text='recordVideo', height=15, width=200, command=startFilming )
-  audioButton = Button(gui, text='Record Audio', height=15, width=200) 
-  sendFilesButton = Button(gui, text='Click Me To Send Files To Server', height=15, width=200) 
-  l.pack()
+# write error handeling
 
-  videoButton.pack(pady=50)
+def sendRecordingsToServer(): 
+  process = subprocess.run('./scripts/send_recordings_to_server.sh',shell=True,capture_output=True)
 
-  audioButton.pack(pady=20) 
+  # idk its stderr and not sdtout
 
-  sendFilesButton.pack(pady=20) 
+  consoleOutput = process.stderr.decode()
 
-  gui.mainloop()
+  if (consoleOutput[0:10] == 'do_connect'): 
+    li['text']='Could not connect to server \n Contact Avery Taylor'
+  else: 
+    li['text'] = consoleOutput 
+    #Assuming its done uploading when it cant output stderr 
+    li['text'] = 'Done uploading' 
+  
 
-main() 
+gui = Tk()
+gui.geometry("3840x2160")
+  
+l = Label(gui, text = "Auto Filmer and Podcaster Desgined by Avery :D", font =("Courier", 20))
+li =  Label(gui, text ='hey', font =("Courier", 20))
+
+
+videoButton = Button(gui, text='Click To Start Filming\n\n(make sure the camera switch is set to record)', height=15, width=200, command=startOrStopFilming)
+audioButton = Button(gui, text='Record Audio', height=15, width=200) 
+sendFilesButton = Button(gui, text='Click Me To Send Files To Server', height=15, width=200, command=sendRecordingsToServer) 
+
+
+l.pack(pady=30)
+
+videoButton.pack(pady=50)
+audioButton.pack(pady=20) 
+sendFilesButton.pack(pady=20) 
+
+li.pack(pady=40) 
+
+gui.mainloop()
+
+
