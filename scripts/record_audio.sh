40736bf6feb18525aba934ba76d4fbc25b4187c7
av <snipfar.1@gmail.com>
2022-01-16 00:01:49 -0800

    fixed pathing issues

--- scripts/record_audio.sh
+++ scripts/record_audio.sh
@@ -1,8 +1,12 @@
-pacmd list-sources | egrep '^\s+name: .*alsa_input'
 
+#pacmd list-sources | egrep '^\s+name: .*alsa_input' # <-- this command is used to find the microphone alias 
 
-parecord --channels=1 -d alsa_input.usb-Focusrite_Scarlett_2i2_USB-00.analog-stereo new.wave
+#assuming where going to use Scarlett 2i2 forever 
+microphone=alsa_input.usb-Focusrite_Scarlett_2i2_USB-00.analog-stereo
 
+parecord --channels=1 -d $microphone /home/av/Desktop/codeStuffs/projects/auto-studio/scripts/new.wave
+
+#make path not static later
 
 
 
--- scripts/send_recordings_to_server.sh
+++ scripts/send_recordings_to_server.sh
@@ -1,6 +1,9 @@
-function downloadRecordings() 
-{
-  gphoto2 -P --new
+
+#saves the files from the camera 
+#the camera doesnt save after its recording because its process gets killed to end reocrding
+
+function downloadVideosFromCamera() {
+  gphoto2 -P --new --filename "../recordings/%f.mp4"
 }
 
 
@@ -11,4 +14,4 @@ function sendRecordingsToServer()
 }
 
 
-downloadRecordings
+downloadVideosFromCamera
