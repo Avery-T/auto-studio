f5cc03fe93427d263551dd438687676b26a26eed
av <snipfar.1@gmail.com>
2022-01-15 17:19:13 -0800

    refacterd with a class

--- gui.py
+++ gui.py
@@ -1,82 +1,72 @@
-from tkinter import *
-import tkinter.font as font
-import subprocess 
+from tkinter import * 
+import subprocess
 
-# you're going to need to refacter this # 
+root = Tk()
+root.title('Auto Filmer')
+root.geometry("400x400") 
 
-recordClicked = False 
-audioClicked = False
 
-##make one fucntion that starts or stops a service
 
-def startOrStopFilming():  
-  global recordClicked 
- 
-  if not recordClicked:  
-    p = subprocess.Popen('./scripts/record_video.sh', shell=True)
-    recordClicked = True 
-    videoButton['text'] = 'Click To Stop Filming' 
+class Studio:
+    
+    def __init__(self, master):
+      myFrame = Frame(master) 
+      myFrame.pack() 
+
+      self.flmBtn = Button(master, text="click me",command=self.film) 
+      self.flmBtn.pack(pady=20)
+
+      self.audioRecBtn = Button(master, text="Start a solo podcast", command=self.recordAudio) 
+      self.audioRecBtn.pack(pady=20)
+
+      self.sendFiles = Button(master, text="Send Files To Server", command=self.sendFilesToServer)  
+      self.sendFiles.pack(pady=20)
+      
+      self.sendFilesLabel = Label(master, text = "", font =("Courier", 20))
+      self.sendFilesLabel.pack()
+       
+
+      self.filmBtnClicked = False  
+      self.audioRecBtnClicked = False 
+      self.sendBtnclicked  = False
+      
+    def film(self): 
+
+      if not self.filmBtnClicked:
+        p = subprocess.Popen('./scripts/record_video.sh', shell=True)
+        self.filmBtnClicked = True
+        self.filmBtn['text'] = 'Click To Stop Filming'
+        
+      else:
+        subprocess.run('kill $(pgrep gphoto2)', shell=True)
+        self.filmBtnClicked = False
+        self.filmBtn['text'] = 'Click To Start Filming'
+
+    def recordAudio(self):
+      if not self.audioRecBtnClicked:
+        p = subprocess.Popen('./scripts/record_audio.sh', shell=True)
+        self.audioRecBtnClicked = True
+        self.audioRecBtn['text'] = 'Click To Start Podcasting'
     #resets the uploading text on new action
-    li['text'] = '' 
-  else:
-    subprocess.run('kill $(pgrep gphoto2)', shell=True) 
-    recordClicked = False
-    videoButton['text'] = 'Click To Start Filming'
+      else:
+        subprocess.run('kill $(pgrep parecord)', shell=True)
+        self.audioRecBtnClicked = False
+        self.audioRecBtn['text'] = 'Click To Start Podcasting'
 
-def startOrStopAudio():
-  global audioClciked 
- 
-  if not recordClicked:  
-    p = subprocess.Popen('./scripts/record_audio.sh', shell=True)
-    audioClicked = True 
-    audioButton['text'] = 'Click To Start Podcasting' 
-    #resets the uploading text on new action
-    li['text'] = '' 
-  else:
-    subprocess.run('kill $(pgrep parecord)', shell=True) 
-    aduidoClicked = False
-    videoButton['text'] = 'Click To Start Podcasting'
+    
+    def sendFilesToServer(self): 
+      process = subprocess.run('./scripts/send_recordings_to_server.sh',shell=True,capture_output=True) 
+      consoleOutput = process.stderr.decode() # idk its stderr and not sdtout
+
+      if (consoleOutput[0:10] == 'do_connect'):
+        self.sendFilesLabel['text']='Could not connect to server \n Contact Avery Taylor'
+      else:
+        self.sendFilesLabel['text'] = consoleOutput
+        #Assuming its done uploading when it cant output stderr 
+        self.sendFilesLabel['text'] = 'Done uploading'
 
 
 
-
-def sendRecordingsToServer(): 
-  process = subprocess.run('./scripts/send_recordings_to_server.sh',shell=True,capture_output=True)
-
-  # idk its stderr and not sdtout
-
-  consoleOutput = process.stderr.decode()
-
-  if (consoleOutput[0:10] == 'do_connect'): 
-    li['text']='Could not connect to server \n Contact Avery Taylor'
-  else: 
-    li['text'] = consoleOutput 
-    #Assuming its done uploading when it cant output stderr 
-    li['text'] = 'Done uploading' 
   
-
-gui = Tk()
-gui.geometry("1366x768")
-  
-l = Label(gui, text = "Auto Studio", font =("Courier", 20))
-li =  Label(gui, text ='hey', font =("Courier", 20))
-
-
-videoButton = Button(gui, text='Click To Start Filming\n\n(make sure the camera switch is set to record)', height=15, width=200, command=startOrStopFilming)
-
-audioButton = Button(gui, text='Record Audio', height=15, width=200, command=startOrStopAudio) 
-
-sendFilesButton = Button(gui, text='Send Files To Server', height=15, width=200, command=sendRecordingsToServer) 
-
-
-l.pack(pady=30)
-
-videoButton.pack()
-audioButton.pack() 
-sendFilesButton.pack(pady=20) 
-
-li.pack(pady=40) 
-
-gui.mainloop()
-
-
+studio = Studio(root) 
+root.mainloop()
