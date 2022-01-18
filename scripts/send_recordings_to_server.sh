febd0376b32a7fe2ac151234ef8dfecdd5b2437f
Avery <snipfar.1@gmail.com>
2022-01-18 00:56:02 -0800

    added recordings folder and fixed pathing to it

--- scripts/send_recordings_to_server.sh
+++ scripts/send_recordings_to_server.sh
@@ -3,7 +3,7 @@
 #the camera doesnt save after its recording because its process gets killed to end reocrding
 
 function downloadVideosFromCamera() {
-  gphoto2 -P --new --filename "../recordings/%f.mp4"
+  gphoto2 -P --new --filename "./recordings/%f.mp4"
 }
 
 
