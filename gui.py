6af80c7af4c3cdc1afb296d15c126d3c35ba7e1a
Avery <snipfar.1@gmail.com>
2022-02-18 12:26:45 -0800

    added shebang

--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from tkinter import * 
 from tkinter import messagebox
 import subprocess
@@ -89,7 +91,9 @@ class Studio:
 
     def sendFilesToServer(self): 
     
-      messagebox.askokcancel("send files","Warning Sending files will halt the program until all the files are sent, this could take hours") 
+      response = messagebox.askokcancel("send files",
+            "Warning Sending files will halt the program until all the files are sent, this could take hours") 
+      if not response: return 
       self.sendFilesLabel['text'] = 'Server is starting...\nplease wait 3 minutes to start file transfer' 
       process = subprocess.run('./scripts/send_recordings_to_server.sh', shell=True, capture_output=True) 
       consoleOutput = process.stdout.decode() 
