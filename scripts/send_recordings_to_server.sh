2abcbe25aa4504171f19a52c9494319a2a2070fa
avery <snipfar.1@gmail.com>
2022-06-04 17:28:03 -0700

    moves files to archive so it doesnt resend files

--- scripts/send_recordings_to_server.sh
+++ scripts/send_recordings_to_server.sh
@@ -1,15 +1,20 @@
 #change pathing in the future 
 
-function connectToVpn() { 
+function connectToVpn() {
   sudo wg-quick up peer5 
 } 
 
 
 function sendRecordingsToServer()
 {
+  #turns file server on
   python /home/av/Projects/auto-studio/scripts/file_server_ctrl.py on 2> /dev/null #to get rid of stderr
   sleep 2m  #it takes time for the server to start
   smbclient \\\\10.0.0.80\\SommersMedia -U='' --password '' -c 'cd Raw\ ; lcd /home/av/Projects/auto-studio/recordings/; prompt; mput *'
+  #need to make a check to make sure the above command worked
+  #moves all the recordings to the archive so the program doesnt resend the same files
+  mv /home/av/Projects/auto-studio/recordings/*  /home/av/Desktop/recordings-archive/
+  #turns file server off
   python /home/av/Projects/auto-studio/scripts/file_server_ctrl.py off 2> /dev/null 
 }
 
