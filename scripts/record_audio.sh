101942a0d8b6a052bbfbc4aa0e6057a6360f6082
Avery <snipfar.1@gmail.com>
2022-06-07 13:10:08 -0700

    this is the correct implementation

--- scripts/record_audio.sh
+++ scripts/record_audio.sh
@@ -1,13 +1,15 @@
-
 #pacmd list-sources | egrep '^\s+name: .*alsa_input' # <-- this command is used to find the microphone alias 
 
 #assuming where going to use Scarlett 2i2 forever 
-microphone=alsa_input.usb-Focusrite_Scarlett_2i2_USB-00.analog-stereo
-
-parecord --channels=1 -d $microphone /home/av/Desktop/codeStuffs/projects/auto-studio/scripts/new.wave
+#parecord --channels=1 -D alsa_input.usb-Focusrite_Scarlett_2i2_USB-00.analog-stereo /home/av/Desktop/codeStuffs/projects/auto-studio/new.wave
 
 #make path not static later
 
+
+
+
+
+parecord --channels=1 -d alsa_input.usb-Focusrite_Scarlett_2i2_USB-00.analog-stereo /home/av/Projects/auto-studio/recordings/$(date +%Y%m%dT%H%M%S).wav
 
 
 
