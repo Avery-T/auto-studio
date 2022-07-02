8f9d8ebfac35f8aa3b42b114cda3ac39747dc363
av <avery.taylor00@gmail.com>
2022-07-01 17:25:41 -0700

    i changed somthing

--- gui.py
+++ gui.py
@@ -6,42 +6,42 @@ import subprocess
 import os 
 import time
 from time import sleep
+
 root = Tk()
 root.title('Auto Filmer')
 root.geometry("1080x1080") 
            
-TEXT = [
-         ['click to start fliming','click to stop fliming', 
-             'camera not detected', 
-              'downloading video from camera'],
-         ['start a solo podcast','audio equipment not detected','click to stop recording auido'], 
-         ['send files to server'], 
-         ['check for updates'], 
-         ['Updating closes the program. \nreopen the program after it closes'],  
-         ['not connected to the internet'] 
-       ]
+
+GUI_TEXT = {
+            'film_options': ['click to start fliming','click to stop fliming', 'camera not detected', 'downloading video from camera'],
+            'podcast_options': ['start a solo podcast','audio equipment not detected','click to stop recording auido'],
+            'internet_options':['send files to server','not connected to the internet'],
+            'update_options': ['check for updates','Updating closes the program. \nreopen the program after it closes']
+        }
+
+# END NIMA's ATTEMPT
 
 class Studio:
     
     def __init__(self, master): 
       myFrame = Frame(master) 
       myFrame.pack() 
-      self.filmBtn = Button(master, text=TEXT[0][0],command=self.film) 
+      self.filmBtn = Button(master, text=GUI_TEXT['film_options'][0],command=self.film) 
       self.filmBtn.pack(pady=20)
 
-      self.audioRecBtn = Button(master, text=TEXT[1][0], command=self.recordAudio) 
+      self.audioRecBtn = Button(master, text=GUI_TEXT['podcast_options'][0], command=self.recordAudio) 
       self.audioRecBtn.pack(pady=20)
 
-      self.sendFilesBtn = Button(master, text=TEXT[2][0], command=self.sendFilesToServer)  
+      self.sendFilesBtn = Button(master, text=GUI_TEXT['internet_options'][0], command=self.sendFilesToServer)  
       self.sendFilesBtn.pack(pady=20)
       
       self.sendFilesLabel = Label(master, text = "", font =("Courier"))
       self.sendFilesLabel.pack()
       
-      self.updateBtn = Button(master, text=TEXT[3][0],command=self.updateCheck) 
+      self.updateBtn = Button(master, text=GUI_TEXT['update_options'][0],command=self.updateCheck) 
       self.updateBtn.pack(pady=50)
       
-      self.filmLabel = Label(master, text =TEXT[4][0], font =("Courier"))
+      self.filmLabel = Label(master, text =GUI_TEXT['update_options'][1], font =("Courier"))
       self.filmLabel.pack()
 
       self.filmBtnClicked = False  
@@ -63,18 +63,18 @@ class Studio:
 
         if runing:  
           self.filmBtnClicked = True
-          self.filmBtn['text'] = TEXT[0][1]
+          self.filmBtn['text'] = GUI_TEXT['film_options'][1]
           self.filmLabel['text'] = '' 
 
         else: # camera not detected
-          self.filmBtn['text'] = TEXT[0][2]
+          self.filmBtn['text'] = GUI_TEXT['film_options'][2]
           #each change needs a function, using after so you dont need to sleep the program
-          self.filmBtn.after(2000, lambda: self.filmBtn.configure(text=TEXT[0][0]))
+          self.filmBtn.after(2000, lambda: self.filmBtn.configure(text=GUI_TEXT['film_options'][0]))
       
       else:
         subprocess.run('kill $(pgrep gphoto2)', shell=True)
         sleep(5)
-        self.filmBtn['text'] = TEXT[0][3]
+        self.filmBtn['text'] = GUI_TEXT['film_options'][3]
         subprocess.run('./scripts/download_video.sh', shell=True) 
         #add code to donwload the video 
          
@@ -94,29 +94,32 @@ class Studio:
 
         if runing:
           self.audioRecBtnClicked = True
-          self.audioRecBtn['text'] = TEXT[1][2] 
+          self.audioRecBtn['text'] = GUI_TEXT['podcast_options'][2] 
         else:
-          self.audioRecBtn.config(text=TEXT[1][1]) 
-          self.audioRecBtn.after(2000, lambda: self.audioRecBtn.configure(text=TEXT[1][0]))
+          self.audioRecBtn.config(text=GUI_TEXT['podcast_options'][1]) 
+          self.audioRecBtn.after(2000, lambda: self.audioRecBtn.configure(text=GUI_TEXT['podcast_options'][0]))
 
       else:
         subprocess.run('killall parecord', shell=True)
         self.audioRecBtnClicked = False
-        self.audioRecBtn['text'] = TEXT[1][0]
+        self.audioRecBtn['text'] = GUI_TEXT['podcast_options'][0]
 
 
     def sendFilesToServer(self): 
       
       if self.internetCheck(): 
-         self.sendFilesBtn['text'] = TEXT[5][0] 
-         self.sendFilesBtn.after(2000, lambda: self.sendFilesBtn.configure(text=TEXT[2][0]))
+         self.sendFilesBtn['text'] = GUI_TEXT['internet_options'][2] 
+         self.sendFilesBtn.after(2000, lambda: self.sendFilesBtn.configure(text=GUI_TEXT['internet_options'][0]))
          return 
       
       #rewrite the check, it takes too long to check if it worked 
       #self.sendFilesBtn['text'] = 'checking to see you can connect to server please wait 3 minutes' 
+      
+     #dont have the oauth key for the bot right now
+     ###self.turnFileServerOnAndConnect()##
+     #change back to 180
+      sleep(30)
 
-      self.turnFileServerOnAndConnect() 
-      time.sleep(185)
       subprocess.run('./scripts/connect_to_vpn.sh', shell=True) 
 
       if self.fileServerIsOn(): 
@@ -127,25 +130,11 @@ class Studio:
        # if not response: return 
        # self.sendFilesBtn.after(1000, lambda: self.sendFilesBtn.configure(text='server in on!'))
 
-
-        
         self.sendFilesLabel['text'] = 'Server is starting...\nplease wait 3 minutes to start file transfer' 
         process = subprocess.run('./scripts/send_recordings_to_server.sh', shell=True, capture_output=True) 
         
         
-        #TO DO display the uploading videos
-        #consoleOutput = process.stdout.decode()
-        #messagebox.showinfo('files being sent',consoleOutput)
-
-        #self.sendFilesLabel['text'] = consoleOutput
-        self.sendFilesBtn.after(1000, lambda: self.sendFilesBtn.configure(text=TEXT[2][0]))
-        self.sendFilesLabel['text'] = 'Done uploading' 
-       
-      else: 
-        lambda: self.sendFilesBtn.configure(text='Cannot connect to server right now')
-        self.sendFilesBtn.after(2000, lambda: self.sendFilesBtn.configure(text=TEXT[2][0]))
-
-
+        #TO DO display the uploading video
     def turnFileServerOnAndConnect(self): 
         subprocess.Popen('./scripts/turn_server_on_and_connect.sh', shell=True)
 
@@ -177,7 +166,7 @@ class Studio:
         self.updateBtn['text'] = 'No updates avaliable' 
       return
   
-
+    #internet check doent work if the program is connected to the vpn
     def internetCheck(self): 
      
        process = subprocess.Popen('./scripts/internet_check.sh', shell=True, stdout=subprocess.PIPE)
--- scripts/send_recordings_to_server.sh
+++ scripts/send_recordings_to_server.sh
@@ -1,16 +1,8 @@
-#change pathing in the future 
-
-function connectToVpn() {
-  sudo wg-quick up peer5 
-} 
-
-#somthing is wrong but i can fix it on the main computer 
 
 function sendRecordingsToServer()
 {
-
-
-  smbclient \\\\10.0.0.80\\SommersMedia -U='' --password '' -c 'cd Raw\ ; lcd ~/Projects/auto-studio/recordings/ ; prompt; mput *'
+  #have to do full path for lcd
+  smbclient \\\\10.0.0.80\\SommersMedia -U='' --password '' -c 'cd Raw\ ; lcd /home/av/Projects/auto-studio/recordings/ ; prompt; mput *'
 
   #need to make a check to make sure the above command worked
   #moves all the recordings to the archive so the program doesnt resend the same files
@@ -18,8 +10,9 @@ function sendRecordingsToServer()
   mv ~/Projects/auto-studio/recordings/*  ~/Desktop/recordings-archive/
  
   #turns file server off
-  python ~/Projects/auto-studio/scripts/file_server_ctrl.py off 2> /dev/null 
+  #python ~/Projects/auto-studio/scripts/file_server_ctrl.py off 2> /dev/nullmb
 }
 
 
-sendRecordingsToServer 
+sendRecordingsToServer
+
